43a2fc0bddc36503b4d072dd59a6b2b3b147934c
sunghyuneun <gregoryeun@gmail.com>
2026-09-16 19:10:13 -0700

    add keymaps to stage and unstage

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Stage the current buffer/file
+vim.keymap.set("n", "<leader>ga", function()
+  require("gitsigns").stage_buffer()
+end, { desc = "Stage current file" })
+
+-- Unstage the current buffer/file
+vim.keymap.set("n", "<leader>gA", function()
+  require("gitsigns").reset_buffer_index()
+end, { desc = "Unstage current file" })
